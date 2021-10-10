96d7f4035e3070f110ba3ffb396dfa76d8b0e5b0
syhien <syhien@outlook.at>
2021-10-10 21:42:58 +0800

    make ui

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.display.text = "0"
     }
-
-
+    var inputing = false;
+    var dot = false;
+    @IBAction func numberAction(_ sender: UIButton) {
+        //
+        let buttonTitle = sender.titleLabel!.text!
+        print(buttonTitle)
+        if(!inputing){
+            if(buttonTitle == "."){
+                // we have to guarantee when inputing == false, then dot = false
+                dot = true;
+                display.text = "0."
+                inputing = true;
+            }else if(buttonTitle != "0"){
+                inputing = true
+                display.text = buttonTitle
+            }
+        }else{
+            if(buttonTitle != "." || dot == false){
+                display.text! = display.text! + buttonTitle
+            }
+        }
+    }
+    
+    @IBAction func operatorAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearAction() {
+    }
+    
+    @IBAction func calcAction() {
+    }
 }
 
